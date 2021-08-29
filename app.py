ce7a3a7c8245ae5c86a6ff368049d98254faacff
Raihan Muzzammil <raihanmuzzammil@gmail.com>
2021-08-29 19:52:03 +0530

    Lessgooo! It works!!

--- app.py
+++ app.py
@@ -1,27 +1,43 @@
-from converter import convert
-from flask import Flask, render_template, request, send_file
+import os
+from werkzeug.utils import secure_filename
+from flask import Flask, render_template, request, send_from_directory, current_app
+import subprocess as sp
+FFMPEG = "C:\\ffmpeg\\bin\\ffmpeg.exe"
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
     return render_template("Index.html")
 
+
 @app.route("/convert")
-def convert():
+def audio():
     return render_template("m4atowav.html")
 
-@app.route("/generate", methods=["POST"])
+
+@app.route("/m4atowav", methods=["POST"])
 def generate():
-    global song
-    song = request.form["audio"]
+    global dfile
+    file = request.files['audio']
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.instance_path, 'uploads', filename))
+    dfile = '{}.{}'.format(os.path.splitext(filename)[0], ".wav")
+    inp = os.path.join(app.instance_path, 'uploads', filename)
+    out = os.path.join(app.instance_path, 'downloads', dfile)
+    convertcmd = [FFMPEG, '-i', inp, '-y', out]
+    downl = sp.Popen(convertcmd,shell=True)
+    try:
+        outs, errs = downl.communicate(timeout=10)
+    except TimeoutError_:
+        proc.kill()
     return render_template("Download.html")
+
 
 
 @app.route("/download")
 def change():
-    convert(song)
-    return send_file(path_or_file="Desktop",download_name=song + ".wav", as_attachment=True)
+    ddir = os.path.join(current_app.root_path, app.instance_path, 'downloads')
+    return send_from_directory(directory=ddir, path="instance\\downloads", filename=dfile, as_attachment=True)
 
 
 @app.route("/random")
--- converter.py
+++
@@ -1,4 +0,0 @@
-from pydub import AudioSegment
-def convert(src):
-    converted = AudioSegment.from_file(src + ".m4a",format="m4a")
-    converted.export(format="wav",path="Desktop")
